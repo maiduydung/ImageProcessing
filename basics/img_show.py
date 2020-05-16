fe349d65c5453d0319c6df538cba5e190c6a4f4f
Maiduydung <maiduydung@yahoo.com>
2020-05-16 18:25:30 +0900

    adding and showing image with matplotlib

---
+++ basics/img_show.py
@@ -0,0 +1,9 @@
+#load and print image
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('resources/cards.jpg')
+plt.imshow(img, interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
