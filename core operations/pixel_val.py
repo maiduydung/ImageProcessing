c55fe4d83c42b2fea0418954395a64747a0f93e7
Maiduydung <maiduydung@yahoo.com>
2020-05-16 20:16:37 +0900

    getting and assigning values

---
+++ core operations/pixel_val.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('resources/messi.jpg')
+#w, h, and channel
+print("shape: ",img.shape)
+
+print("number of pixel: ",img.size)
+print("type: ", img.dtype)
+
+
+ball = img[280:340, 330:390]
+print("ball ",ball)
+img[273:333, 000:60] = ball
+
+
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+
+
+# 552 292
+# 668 405
