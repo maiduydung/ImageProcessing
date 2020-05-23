74bdafd5e66d2ba959aba2c8e0b1d146f0ad3bde
Mai Duy Dũng <maiduydung@yahoo.com>
2020-05-23 21:39:25 +0900

    iris dataset, basic

---
+++ basics/decision tree/demo.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import tree
+
+iris = load_iris()
+
+test_idx = [0, 50, 100]
+
+#removing testing data out of training data, to see how it handles data never seen before
+#iris
+train_target = np.delete(iris.target, test_idx)
+
+#iris features
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+#testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print(test_target)
+print(clf.predict(test_data))
+
+#viz
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                      feature_names=iris.feature_names,  
+                     class_names=iris.target_names,  
+                     filled=True, rounded=True,  
+                     special_characters=True) 
+graph = graphviz.Source(dot_data) 
+graph.render("iris") 
