48c5db8ff48efab0d9e16f21b0783b735f8c05df
Mai Duy Dũng <maiduydung@yahoo.com>
2020-05-31 12:00:06 +0900

    resize

--- core operations/object_tracking.py
+++ core operations/object_tracking.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('../resources/trump.mp4')
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
+        cv2.namedWindow("before", cv2.WINDOW_NORMAL)
         cv2.imshow("before", frame)
+
+
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV ) 
+        cv2.namedWindow("after", cv2.WINDOW_NORMAL)
         cv2.imshow("after", frame)
         
         if cv2.waitKey(25) & 0xFF == ord('q'):
